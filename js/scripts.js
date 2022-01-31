5f6d53c2809dc41ed41841a8b10ffa952c79c7ff
Amiya Stillwell <stillwell.amiya@gmail.com>
2022-01-30 18:53:41 -0800

    added docu.ready(function) to js script for index2 file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,47 @@
 
-function survey(faveNumberInput) {
-  return faveNumberInput;
-}
+// function survey(faveNumberInput) {
+//   return faveNumberInput;
+// }
 
 
 
-$(document).ready(function() {
-  $("button#red").click(function() {
-    $("body").removeClass();
-    $("body").addClass("red-background");
-  });
-  $(".clickable").click(function() {
-    $("#survey-showing").toggle();
-    $("#survey-hidden").toggle();
-    $("#python").hide();
-    $("#go").hide();
-    $("#swift").hide();
+// $(document).ready(function() {
+//   $("button#red").click(function() {
+//     $("body").removeClass();
+//     $("body").addClass("red-background");
+//   });
+//   $(".clickable").click(function() {
+//     $("#survey-showing").toggle();
+//     $("#survey-hidden").toggle();
+//     $("#python").hide();
+//     $("#go").hide();
+//     $("#swift").hide();
 
-  });
-});
+//   });
+// });
 
 
-$(document).ready(function() {
-  $("#formOne").submit(function(event) {
-    event.preventDefault();
-const person1Input = $("input#person1").val();    
-const color1Input = $("input#color1").val();
-const dob = $("#born").val();
-const flavor = $("input:radio[name=flavor]:checked").val();
-const faveNumber = parseInt($('faveNumber').val());
-const result = faveNumber(faveNumberInput) {
-  if (faveNumber >= 25) {
-    $(#python).show();
-  } else if (faveNumber === 50 || faveNumber < 65) {
-    $(#go).show();
-  } else {
-    $(#swift).show();
-  }
-};
-alert(result).concat(" Is this your final answer?") {
-  confirm(prompt("Are you sure? Click OK for yes or Cancel for no."))};
-  });
-});
+// $(document).ready(function() {
+//   $("#formOne").submit(function(event) {
+//     event.preventDefault();
+// const person1Input = $("input#person1").val();    
+// const color1Input = $("input#color1").val();
+// const dob = $("#born").val();
+// const flavor = $("input:radio[name=flavor]:checked").val();
+// const faveNumber = parseInt($('faveNumber').val());
+// const result = faveNumber(faveNumberInput) {
+//   if (faveNumber >= 25) {
+//     $(#python).show();
+//   } else if (faveNumber === 50 || faveNumber < 65) {
+//     $(#go).show();
+//   } else {
+//     $(#swift).show();
+//   }
+// };
+// alert(result).concat(" Is this your final answer?") {
+//   confirm(prompt("Are you sure? Click OK for yes or Cancel for no."))};
+//   });
+// });
 
 
 
@@ -57,5 +57,16 @@ alert(result).concat(" Is this your final answer?") {
  // });
 
 
+ $(document).ready(function() {
+  $("#formOne").submit(function() {
+    $(".person1").append("blah blah");
+    $(".person2").append("blah blah");
+    $(".animal").append("blah blah");
+    $(".exclamation").append("blah blah");
+    $(".verb").append("blah blah");
+    $(".noun").append("blah blah");
 
+    $("#story").show();
+  });
+});
 
